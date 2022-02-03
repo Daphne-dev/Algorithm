47cdfb5c2e6da2254230ed1a1ca4cdd685381ab6
Daphnedev <>
2022-02-03 16:40:10 +0000

    feat: 프로그래머스 level 2 문제 4개 추가
        - level 2 124 나라의 숫자
        - level 2 가장 큰 정사각형
        - level 2 프린터
        - level 2 오픈채팅방

---
+++ programmers/2_dynamic_programming/Level_2_가장_큰_정사각형.js
@@ -0,0 +1,28 @@
+/**
+ * board에서 자신의 위치의 왼쪽, 왼쪽 상단, 위쪽의 숫자를 비교하여 
+ * 해당 위치에서 만들 수 있는 가장 큰 정사각형의 크기를 메모이제이션하는 방법으로 해결
+ */
+function solution(board)
+{
+    let answer = 0;
+    const row = board.length;
+    const col = board[0].length;
+    
+    if (row < 2 || col < 2) return 1;
+    
+    for (let i = 1; i < row; i++) {
+        for (let j = 1; j < col; j++) {
+            if (board[i][j] !== 0) {
+                let min = Math.min(
+                    board[i][j -1],
+                    board[i -1][j -1],
+                    board[i -1][j]
+                );
+                board[i][j] = min + 1;
+            }
+        if (answer < board[i][j]) answer = board[i][j];
+        }
+    }
+
+    return answer * answer;
+}
---
+++ programmers/3_스택_큐/Level_2_프린터.js
@@ -0,0 +1,29 @@
+function solution(priorities, location) {
+    let answer = 0;
+    let highestPriority = Math.max(...priorities);
+    
+    while (true) {
+        let value = priorities.shift();
+        
+        if (highestPriority === value) {
+            answer++;
+            
+            if (location === 0) {
+                break;
+            } else {
+                location--;
+                highestPriority = Math.max(...priorities);
+            }
+        } else {
+            priorities.push(value);
+            
+            if (location === 0) {
+                location = priorities.length - 1;
+            } else {
+                location--;
+            }
+        }
+        
+    }
+    return answer;
+}
