b30e08e3847a7c537b56311b154321462833e152
Daphne-dev <daphned@naver.com>
2022-02-08 21:49:19 +0900

    fix: prettier 적용

--- programmers/1_구현/Level_2_124_나라의_숫자.js
+++ programmers/1_구현/Level_2_124_나라의_숫자.js
@@ -1,18 +1,18 @@
 function solution(n) {
-    let answer = '';
-    
-    while(n) {
-        let [quotient, remainder] = divmod(n, 3);
-        answer = '412'[remainder] + answer;
-        
-        if(!remainder) {
-            quotient--;
-        }  
-        n = quotient;
-    }
-    return answer;
+	let answer = '';
+
+	while (n) {
+		let [quotient, remainder] = divmod(n, 3);
+		answer = '412'[remainder] + answer;
+
+		if (!remainder) {
+			quotient--;
+		}
+		n = quotient;
+	}
+	return answer;
 }
 
 function divmod(num, targetNum) {
-    return [parseInt(num / targetNum), num % targetNum];
+	return [parseInt(num / targetNum), num % targetNum];
 }
--- programmers/4_해시/Level_2_오픈채팅방.js
+++ programmers/4_해시/Level_2_오픈채팅방.js
@@ -1,26 +1,26 @@
 function solution(record) {
-    const answer = [];
-    const lastUserInfo = {};
-    
-    for (let i = 0; i < record.length; i++) {
-        let [ action, uid, nickname ] = record[i].split(" ");
-        
-        if (action === "Enter" || action === "Change") {
-            lastUserInfo[uid] = { nickname };
-        }
-    }
-    
-    for (let i = 0; i < record.length; i++) {
-        let [ action, uid ] = record[i].split(" ");
-        let nickname = lastUserInfo[uid].nickname;
-        
-        if (action === "Enter") {
-            answer.push(`${nickname}님이 들어왔습니다.`);
-        }
-        
-        if (action === "Leave") {
-            answer.push(`${nickname}님이 나갔습니다.`);
-        }
-    }
-    return answer;
+	const answer = [];
+	const lastUserInfo = {};
+
+	for (let i = 0; i < record.length; i++) {
+		let [action, uid, nickname] = record[i].split(' ');
+
+		if (action === 'Enter' || action === 'Change') {
+			lastUserInfo[uid] = { nickname };
+		}
+	}
+
+	for (let i = 0; i < record.length; i++) {
+		let [action, uid] = record[i].split(' ');
+		let nickname = lastUserInfo[uid].nickname;
+
+		if (action === 'Enter') {
+			answer.push(`${nickname}님이 들어왔습니다.`);
+		}
+
+		if (action === 'Leave') {
+			answer.push(`${nickname}님이 나갔습니다.`);
+		}
+	}
+	return answer;
 }
