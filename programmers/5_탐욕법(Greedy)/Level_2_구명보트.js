784a3483ca022e0a1aa6a55eb3a83542d465de75
Daphne-dev <daphned@naver.com>
2022-02-08 21:49:48 +0900

    feat: 프로그래머스 level 2 구명보트 추가

---
+++ programmers/5_탐욕법(Greedy)/Level_2_구명보트.js
@@ -0,0 +1,19 @@
+function solution(people, limit) {
+	people.sort((a, b) => a - b);
+
+	let [min, max] = [0, people.length - 1];
+	let answer = 0;
+
+	while (min < max) {
+		if (people[min] + people[max] <= limit) {
+			min++;
+			max--;
+		} else {
+			max--;
+		}
+		answer++;
+	}
+
+	if (min === max) answer++;
+	return answer;
+}
